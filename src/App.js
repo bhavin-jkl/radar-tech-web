7f145f13beed0a9297973a1c106e20700e81841c
bhavin-jkl <bhavin.rasadiya@jklgroup.in>
2023-06-29 21:47:31 +0530

    Resolved the issue

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-3xl bg-slate-600 underline">Hello world!</h1>;
+  return <></>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // content: [
-  //   "./src/**/*.{js,jsx,ts,tsx}",
-  //   "./public/**/*.{js,jsx,ts,tsx,html}",
-  // ],
-  content: ['./src/**/*.js', './public/index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//     "./public/**/*.{js,jsx,ts,tsx,html}",
+//   ],
+//   // content: ['./src/**/*.js', './public/index.html'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
